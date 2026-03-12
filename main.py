bfefa522e13e9fc519fb55e7c6ff2c6361f35c74
Mallisavitha <2k21it34@kiot.ac.in>
2026-03-12 12:47:58 +0530

    Malli commit

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-<<<<<<< HEAD
+
 print("hello")
 print("monika")
-=======
 print("Nan dhan da diver uhh")
 print("hello")
 print("monika")
 
->>>>>>> 91e21a541ef477cac5a8de4ddb8cafcfb0ad0279
 print("hello")
 print("Elakkiya")
 print("ram//")
 print("hello")
 print("Hi...")
 print("Preethi")
-<<<<<<< HEAD
-print("Niketana")
-print("Preethi")
-print("Niketana")
-print("Hi its Preethi")
-=======
 
 print("Niketana")
 print("Preethi")
 print("Niketana")
->>>>>>> 91e21a541ef477cac5a8de4ddb8cafcfb0ad0279
+print("Hi its Preethi")
+
+
+print("Niketana")
+print("Preethi")
+print("Niketana")
+
+print("Malli")
