91e21a541ef477cac5a8de4ddb8cafcfb0ad0279
Ramkishore S N <ramkishoresn@gmail.com>
2026-03-12 12:41:13 +0530

    resolved merge conflict

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+print("Nan dhan da diver uhh")
 print("hello")
 print("monika")
-=======
 
 print("hello")
 print("Elakkiya")
@@ -9,11 +8,7 @@ print("ram//")
 print("hello")
 print("Hi...")
 print("Preethi")
-<<<<<<< HEAD
 
->>>>>>> e06aa59334e7b636d3a37d4c19b339e03fb920c1
-=======
 print("Niketana")
 print("Preethi")
 print("Niketana")
->>>>>>> 85bef8fe47ef215ad32eb68b2a8b5aafa9ad00c1
