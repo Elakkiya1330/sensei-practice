11c07a937acbbd8838f0f7f1d3e4cd46cf366e26
impreethig <preethig1604@gmail.com>
2026-03-12 12:42:34 +0530

    edited main again-Preethi

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 print("hello")
 print("monika")
-=======
-
 print("hello")
 print("Elakkiya")
 print("ram//")
 print("hello")
 print("Hi...")
 print("Preethi")
-<<<<<<< HEAD
-
->>>>>>> e06aa59334e7b636d3a37d4c19b339e03fb920c1
-=======
 print("Niketana")
 print("Preethi")
 print("Niketana")
->>>>>>> 85bef8fe47ef215ad32eb68b2a8b5aafa9ad00c1
+print("Hi its Preethi")
