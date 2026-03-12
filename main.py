c8c69a528021e281a6de3bf4ca2589416e6d4dc0
Elakkiya1330 <elakkiya913270@gmail.com>
2026-03-12 15:12:36 +0530

    summa edit

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
 print("Nan dhan da diver uhh")
 print("hello")
-print("monika")
-
-print("hello")
-print("monika")
-print("Nan dhan da diver uhh")
-print("hello")
-print("monika")
-
-print("hello")
 print("Elakkiya")
 print("ram//")
 print("hello")
 print("Hi...")
-print("Preethi")
-
-print("Niketana")
-print("Preethi")
-print("Niketana")
 print("mass moni")
 print("Hi its Preethi")
-
-
 print("Niketana")
-print("Preethi")
-print("Niketana")
-
 print("Malli")
 print("js")
+print("elakkiya check")
